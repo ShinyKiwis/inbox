2b86b8da8aa574f0ccc6e3a5218ab341824a59ff
Nguyen Hua <nguyenhua.hcm@gmail.com>
2024-06-23 22:48:03 +0700

    Fix wrong header placement for devise routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  layout :layout_by_resource
 
-  protected
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,3 @@
-# frozen_string_literal: true
-
 module ApplicationHelper
-  AUTH_PATHS = %i[new_user_session new_user_registration new_user_password].freeze
-  def render_header
-    render 'layouts/header' unless current_page_any?(AUTH_PATHS)
-  end
-
-  private
-
-  def current_page_any?(paths)
-    paths.any? { |path| current_page?(path) }
-  end
 end
+
