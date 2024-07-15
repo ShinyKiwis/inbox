eae1de55f683522973f47fb6f26b21b9ea750862
Nguyen Hua <nguyenhua.hcm@gmail.com>
2024-07-15 22:52:21 +0700

    Implement alert modal and fa_icon helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+
+  def fa_icon(name)
+    parsed_name = name.to_s.tr("_","-")
+    tag.i class: "fa-solid fa-#{parsed_name}"
+  end
 end
+
