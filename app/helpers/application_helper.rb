f2067f60ac2a4056d05ea357e93f3af1e877e3db
Nguyen Hua <nguyenhua.hcm@gmail.com>
2024-07-29 23:24:30 +0700

    Restructure css folders and implement tooltip

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,11 @@ module ApplicationHelper
   def fa_icon(name)
     tag.i class: "fa-solid fa-#{name}"
   end
+
+  def tooltip(text, &block)
+    content_tag :div, class: 'tooltip' do
+      concat capture(&block)
+      concat content_tag :span, text, class: 'tooltip-text'
+    end
+  end
 end
